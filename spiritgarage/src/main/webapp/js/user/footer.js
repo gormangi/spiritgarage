e2b0b1ae971465c596629d4366d46a9638a0688b
gormangi <gormangi@naver.com>
2020-06-08 00:23:29 +0900

    cd

--- spiritgarage/src/main/webapp/js/user/footer.js
+++ spiritgarage/src/main/webapp/js/user/footer.js
@@ -11,39 +11,62 @@ var footerFn = {
 				dataType : 'json',
 				type : 'post',
 				success : function(res){
+					console.log(res);
 					
-					if(res.location.content){
-						$("#main_footer_location").text(res.location.content);
-					}else{
+					if(!res.location){
 						$("#main_footer_location").text('(14961)경기도 시흥시 포동 20-19 스피릿개러지');
+					}else{
+						if(res.location.content){
+							$("#main_footer_location").text(res.location.content);
+						}else{
+							$("#main_footer_location").text('(14961)경기도 시흥시 포동 20-19 스피릿개러지');
+						}
 					}
 					
-					if(res.mobile.content){
-						$("#main_footer_mobile").text(res.mobile.content);
-						$("#main_footer_mobile").attr('href','tel:'+res.mobile.content);
-					}else{
+					if(!res.mobile){
 						$("#main_footer_mobile").text('010-7476-7884');
 						$("#main_footer_mobile").attr('href','tel:010-7476-7884');
+					}else{
+						if(res.mobile.content){
+							$("#main_footer_mobile").text(res.mobile.content);
+							$("#main_footer_mobile").attr('href','tel:'+res.mobile.content);
+						}else{
+							$("#main_footer_mobile").text('010-7476-7884');
+							$("#main_footer_mobile").attr('href','tel:010-7476-7884');
+						}
 					}
 					
-					if(res.phone.content){
-						$("#main_footer_phone").text(res.phone.content);
-						$("#main_footer_phone").attr('href','tel:'+res.phone.content);
-					}else{
+					if(!res.phone){
 						$("#main_footer_phone").text('031-314-8884');
 						$("#main_footer_phone").attr('href','tel:031-314-8884');
+					}else{
+						if(res.phone.content){
+							$("#main_footer_phone").text(res.phone.content);
+							$("#main_footer_phone").attr('href','tel:'+res.phone.content);
+						}else{
+							$("#main_footer_phone").text('031-314-8884');
+							$("#main_footer_phone").attr('href','tel:031-314-8884');
+						}
 					}
 					
-					if(res.openhour.content){
-						$("#main_footer_openhour").html('<pre>'+res.openhour.content+'</pre>');
-					}else{
+					if(!res.openhour){
 						$("#main_footer_openhour").html('월 ~ 토 09:30 - 20:00<br/>일요일은 예약 고객에 한하여 작업합니다');
+					}else{
+						if(res.openhour.content){
+							$("#main_footer_openhour").html('<pre>'+res.openhour.content+'</pre>');
+						}else{
+							$("#main_footer_openhour").html('월 ~ 토 09:30 - 20:00<br/>일요일은 예약 고객에 한하여 작업합니다');
+						}
 					}
 					
-					if(res.mainfield.content){
-						$("#main_footer_mainfield").html('<pre>'+res.mainfield.content+'</pre>');
-					}else{
+					if(!res.mainfield){
 						$("#main_footer_mainfield").html('경정비 | 합성오일 | 휠타이어<br/>블랙박스 | 썬팅 | 네비게이션 | 옵션설치');
+					}else{
+						if(res.mainfield.content){
+							$("#main_footer_mainfield").html('<pre>'+res.mainfield.content+'</pre>');
+						}else{
+							$("#main_footer_mainfield").html('경정비 | 합성오일 | 휠타이어<br/>블랙박스 | 썬팅 | 네비게이션 | 옵션설치');
+						}
 					}
 					
 				}
